63ed6bd25dc2432918ab4f3e54a52c01a5817428
Matt Hwang <mhwang206@gmail.com>
2014-01-18 22:59:29 -0800

    Add player storage array

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
 function gameBoardCtrl($scope){
-	$scope.gameBoard = [['','',''],['','',''],['','','']];
 
-	var xTurn = false;
+	var playerOne = [];
+	var playerTwo = []; 
 
-	$scope.attackCell = function(row, cell){
-		// $scope.gameBoard[row][cell] = xTurn ? 
-		$scope.gameBoard[row][cell] = xTurn ? "X" : "O";
-		xTurn = !xTurn;
-	}
-
+	var winCombo = [[0,1,2],[3,4,5],[6,7,8],
+					[0,3,6],[1,4,7],[2,5,8],
+					[0,4,8],[2,4,6]
+				   ];
 
 
 
+	$scope.gameBoard = ['','','','','','','','',''];
+
+	var xTurn = true;
+
+	$scope.attackCell = function(cell){
+		if ($scope.gameBoard[cell] == "") {
+			$scope.gameBoard[cell] = xTurn ? "X" : "O";
+			xTurn = !xTurn;
+
+			console.log(cell);
+		};
+	};
 }
+
+
+
+	
+
+
+
+
+// Winning combination 
+// 1. winning combination
+// 2. Store clicked cell value in the array 
+// 3. 
 
